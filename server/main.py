e28846202658d792e846abdc9a4dc9b62be8d790
Ivan <vanas0576@gmail.com>
2026-07-30 00:58:25 +0500

    create test server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from server!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Word(BaseModel):
+    game: str
+    id: int
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/start")
+def create_item(item: Word, ):
+    print(item.player, item.word)
+    return {"game_name": item.game, "game_id": item.id}
